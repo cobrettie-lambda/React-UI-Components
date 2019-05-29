5a89020866ff2fbb2ec275640707b14d18329b03
Cobrettie <cobrettiegarner3@gmail.com>
2019-05-29 19:56:55 -0400

    add Footer component, import to App.js

---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className="footerContainer">
+            <div className="footerContent">
+                
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
