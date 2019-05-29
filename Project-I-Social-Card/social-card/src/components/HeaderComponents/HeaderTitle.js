35d07eb90049a7a742d7957c95a3b93bc3e9861e
Cobrettie <cobrettiegarner3@gmail.com>
2019-05-29 17:36:44 -0400

    add headerTitle component

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -5,6 +5,8 @@ const HeaderTitle = () => {
     return (
         <div className="headerWrapper">
             <div className="headerTitle">Lambda School</div>
+            <span>@lambdaschool</span>
+            <span>26 jan</span>
         </div>
     );
 }
