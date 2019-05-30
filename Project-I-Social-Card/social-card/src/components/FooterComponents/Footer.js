1776a880c41b503d2b68f74aaa5b906926bde2fc
Cobrettie <cobrettiegarner3@gmail.com>
2019-05-29 20:58:23 -0400

    add styling to Footer.js icons

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -5,10 +5,12 @@ const Footer = () => {
     return (
         <div className="footerContainer">
             <div className="footerContent">
-                <i class="far fa-comment"></i>
-                <i class="fas fa-sync"></i>
-                <i class="far fa-heart"></i>
-                <i class="far fa-envelope"></i>
+                <div className="footerIcons">
+                    <i class="far fa-comment"></i>
+                    <i class="fas fa-sync"></i>
+                    <i class="far fa-heart"></i>
+                    <i class="far fa-envelope"></i>
+                </div>
             </div>
         </div>
     );
