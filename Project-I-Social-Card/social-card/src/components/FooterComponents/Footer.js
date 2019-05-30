0f8dcd6aaa7c4ba6e79f03171bb3f9d5595c36c1
Cobrettie <cobrettiegarner3@gmail.com>
2019-05-29 22:11:42 -0400

    update Footer.js, continue counter functionality

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -1,15 +1,40 @@
 import React from 'react';
 import './Footer.css';
 
+// let counter = document.getElementById('btncounter'), count = 0;
+// counter = () => {
+//         count += 1;
+//         counter.textContent = " " + count;
+// };
+
+// var likeCount = 0;
+// var retweetCount = 0;
+
+// function addLike() {
+//     likeCount +=1;
+// };
+
+// function addRetweet() {
+//     retweetCount +=1;
+// };
+
+// var count = 0;
+// function myFunction() {
+//     count ++;
+//     var divData = document.getElementById('btnCounter');
+//     divData.innerHTML = count;
+//     console.log(count);
+// }
+
 const Footer = () => {
     return (
         <div className="footerContainer">
             <div className="footerContent">
                 <div className="footerIcons">
-                    <i class="far fa-comment"></i>
-                    <i class="fas fa-sync"></i>
-                    <i class="far fa-heart"></i>
-                    <i class="far fa-envelope"></i>
+                    <button id="btncounter"><i className="far fa-comment"></i></button>
+                    <button id="btncounter"><i className="fas fa-sync"></i></button>
+                    <button id="btncounter"><i className="far fa-heart"></i></button>
+                    <button id="btncounter"><i className="far fa-envelope"></i></button>
                 </div>
             </div>
         </div>
