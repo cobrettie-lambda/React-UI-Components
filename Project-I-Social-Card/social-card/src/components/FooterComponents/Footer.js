0ff820dd1203255467377a44c25bdf28169ffbc6
Cobrettie <cobrettiegarner3@gmail.com>
2019-05-29 20:52:15 -0400

    add icons for footer.js

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -5,7 +5,10 @@ const Footer = () => {
     return (
         <div className="footerContainer">
             <div className="footerContent">
-                
+                <i class="far fa-comment"></i>
+                <i class="fas fa-sync"></i>
+                <i class="far fa-heart"></i>
+                <i class="far fa-envelope"></i>
             </div>
         </div>
     );
