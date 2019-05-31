6ed3571f978e244cc11d4b71fb04dc563616589b
Cobrettie <cobrettiegarner3@gmail.com>
2019-05-31 19:57:58 -0400

    style number, action, clear, zero

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -10,14 +10,25 @@ const App = () => {
       <CalculatorDisplay />
 
       <NumberButton number="clear" buttonStyle="clear" />
-      <ActionButton symbol="/" buttonStyle="action" />
+      <ActionButton symbol="&#247;" buttonStyle="action" />
+
       <NumberButton number="7" buttonStyle="number" />
       <NumberButton number="8" buttonStyle="number" />
       <NumberButton number="9" buttonStyle="number" />
+      <ActionButton symbol="x" buttonStyle="action" />
 
-      
+      <NumberButton number="4" buttonStyle="number" />
+      <NumberButton number="5" buttonStyle="number" />
+      <NumberButton number="6" buttonStyle="number" />
+      <ActionButton symbol="-" buttonStyle="action" />
 
+      <NumberButton number="1" buttonStyle="number" />
+      <NumberButton number="2" buttonStyle="number" />
+      <NumberButton number="3" buttonStyle="number" />
+      <ActionButton symbol="+" buttonStyle="action" />
 
+      <NumberButton number="0" buttonStyle="zero" />
+      <ActionButton symbol="=" buttonStyle="action" />
     </div>
   );
 };
