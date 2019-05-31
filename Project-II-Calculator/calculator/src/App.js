10ee9bbcdc27512513eb6e24f38c0106fbcf19a0
Cobrettie <cobrettiegarner3@gmail.com>
2019-05-31 19:38:27 -0400

    begin building UI

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './App.css';
+import NumberButton from './components/ButtonComponents/NumberButton';
+import ActionButton from './components/ButtonComponents/ActionButton';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
 
 const App = () => {
   return (
-    <div>
+    <div className="app-container">
+      <CalculatorDisplay />
+
+      <NumberButton number="clear" buttonStyle="clear" />
+      <ActionButton symbol="/" buttonStyle="action" />
+      <NumberButton number="7" buttonStyle="number" />
+      <NumberButton number="8" buttonStyle="number" />
+      <NumberButton number="9" buttonStyle="number" />
+
       
+
+
     </div>
   );
 };
